877d3e675cfa86d3ad35c29249aff39fcfd6ec46
Efio-esien Efiom <efioesien@gmail.com>
2023-05-16 13:43:41 +0100

    added the project controller class and created the save or update project endpoint

---
+++ src/main/java/com/example/projectmanagementapp/config/Contact.java
@@ -0,0 +1,9 @@
+package com.example.projectmanagementapp.config;
+
+public class Contact {
+        String name = "project-management";
+        String url = "bobrox44@gmail.com";
+        String email = "bobrox44@gmail.com";
+
+
+}
